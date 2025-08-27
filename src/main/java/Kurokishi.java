0b4cdb51386d1d7fcaca0546db4608b35c6ed8c4
Kurokishi592 <kennethwongcunwi@gmail.com>
2025-08-27 16:17:38 +0800

    add level 0: personalise chatbot name and implement skeletal version

---
+++ src/main/java/Kurokishi.java
@@ -0,0 +1,30 @@
+public class Kurokishi {
+    public static void main(String[] args) {
+        String name = "Kurokishi";
+        String logo =
+                " _  __  \n"
+                        + "| |/ /  \n"
+                        + "| ' /   \n"
+                        + "| . \\   \n"
+                        + "|_|\\_\\  \n"
+                        + " _    _  \n"
+                        + "| |  | | \n"
+                        + "| |  | | \n"
+                        + "| |__| | \n"
+                        + " \\____/  \n"
+                        + " ____   \n"
+                        + "|  _ \\  \n"
+                        + "| |_) | \n"
+                        + "|  _ <  \n"
+                        + "|_| \\_\\ \n"
+                        + "  ____   \n"
+                        + " / __ \\  \n"
+                        + "| |  | | \n"
+                        + "| |__| | \n"
+                        + " \\____/  \n";
+        System.out.println("Hello! I'm " + name);
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(logo);
+    }
+}
