6f59d54747019db3db27621df7922c24c078a663
Kurokishi592 <kennethwongcunwi@gmail.com>
2025-08-27 17:03:34 +0800

    Add level-1: echo feature and exit upon 'bye' input

--- src/main/java/Kurokishi.java
+++ src/main/java/Kurokishi.java
@@ -1,6 +1,10 @@
+import java.util.Scanner;
+
 public class Kurokishi {
     public static void main(String[] args) {
         String name = "Kurokishi";
+        String dash_line ="------------------------------------------------------------";
+        String prompt_for_command = "Please type something:) ";
         String logo =
                         " _  __  \n"
                         + "| |/ /  \n"
@@ -22,9 +26,26 @@ public class Kurokishi {
                         + "| |  | | \n"
                         + "| |__| | \n"
                         + " \\____/  \n";
+        System.out.println(logo);
         System.out.println("Hello! I'm " + name);
         System.out.println("What can I do for you?");
+        System.out.println(dash_line);
+        System.out.println(prompt_for_command);
+
+        // Echos commands by the user until the user types "bye"
+        Scanner in = new Scanner(System.in);
+        String command;
+        do {
+            command = in.nextLine();
+            if (!command.equals("bye")) {
+                System.out.println(dash_line);
+                System.out.println("    " + command);
+                System.out.println(dash_line);
+                System.out.println(prompt_for_command);
+            }
+        } while (!command.equals("bye"));
+        System.out.println(dash_line);
         System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(logo);
+        System.out.println(dash_line);
     }
 }
