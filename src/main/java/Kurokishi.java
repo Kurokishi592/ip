399d281171e9e06fbdb4cfe65a66da5bdc6dda3a
Kurokishi592 <kennethwongcunwi@gmail.com>
2025-09-03 11:07:51 +0800

    Clean up the code for better readibility

--- src/main/java/Kurokishi.java
+++ src/main/java/Kurokishi.java
@@ -1,62 +1,81 @@
 import java.util.Scanner;
 
 public class Kurokishi {
-
+    
     // A simple array to store tasks
     private static String[] taskList = new String[100];
-
+    
     public static void main(String[] args) {
         String name = "Kurokishi";
         String dashLine ="------------------------------------------------------------";
         String promptCommand = ("Simply type your task to add to a list\n" +
-                        "Type 'list' to see your tasks\n" +
-                        "Type 'mark <task number>' to mark a task as done\n" +
-                        "Type 'unmark <task number>' to unmark a task as not done\n" +
-                        "Type 'bye' to exit the program\n"
+        "Type 'list' to see your tasks\n" +
+        "Type 'mark <task number>' to mark a task as done\n" +
+        "Type 'unmark <task number>' to unmark a task as not done\n" +
+        "Type 'bye' to exit the program\n"
         );
         String logo = " _  __  \n"
-                        + "| |/ /  \n"
-                        + "| ' /   \n"
-                        + "| . \\   \n"
-                        + "|_|\\_\\  \n"
-                        + " _    _  \n"
-                        + "| |  | | \n"
-                        + "| |  | | \n"
-                        + "| |__| | \n"
-                        + " \\____/  \n"
-                        + " ____   \n"
-                        + "|  _ \\  \n"
-                        + "| |_) | \n"
-                        + "|  _ <  \n"
-                        + "|_| \\_\\ \n"
-                        + "  ____   \n"
-                        + " / __ \\  \n"
-                        + "| |  | | \n"
-                        + "| |__| | \n"
-                        + " \\____/  \n";
+        + "| |/ /  \n"
+        + "| ' /   \n"
+        + "| . \\   \n"
+        + "|_|\\_\\  \n"
+        + " _    _  \n"
+        + "| |  | | \n"
+        + "| |  | | \n"
+        + "| |__| | \n"
+        + " \\____/  \n"
+        + " ____   \n"
+        + "|  _ \\  \n"
+        + "| |_) | \n"
+        + "|  _ <  \n"
+        + "|_| \\_\\ \n"
+        + "  ____   \n"
+        + " / __ \\  \n"
+        + "| |  | | \n"
+        + "| |__| | \n"
+        + " \\____/  \n";
         System.out.println(logo);
         System.out.println("Hello! I'm " + name +"\n" +
-                    "I can help you keep track of your tasks!\n");
+        "I can help you keep track of your tasks!\n");
         System.out.println(promptCommand);
         System.out.println(dashLine);
-
-        // Perform Task Tracking
+        
+        // Reads command by user and execute corresponding action
         Scanner in = new Scanner(System.in);
         int taskIndex = 0;
         String input;
-        do {
+        
+        while(true) {
             input = in.nextLine();
-            if (!input.equals("bye")) {
+            String[] parts = input.split(" ", 2);
+            String command = parts[0];
+            
+            // Exit the program upon "bye" command
+            if (command.equals("bye")) {
                 System.out.println(dashLine);
-
-                if (input.equals("list")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println(dashLine);
+                break;
+            }
+            
+            System.out.println(dashLine);
+            
+            switch (command) {
+                
+                case "list":
                     System.out.println("Here are the tasks in your list:");
                     for (int i = 0; i < taskIndex; i++) {
                         System.out.println((i + 1) + ". " + taskList[i]);
                     }
-                }
-                else if (input.startsWith("mark ") || input.startsWith("unmark ")) {
-                    String[] parts = input.split(" ");
+                    break;
+                
+                case "mark":
+                case "unmark":
+                    // Handle unexpected input
+                    if (parts.length < 2) {
+                        System.out.println("    Please specify a task number.");
+                        break;
+                    }
                     int taskNumber = Integer.parseInt(parts[1]) - 1; // index zero stuff
                     if (taskNumber >= 0 && taskNumber < taskIndex) {
                         String taskToMark = taskList[taskNumber];
@@ -73,21 +92,22 @@ public class Kurokishi {
                     } else {
                         System.out.println("    Invalid task number.");
                     }
-                } else {
+                    break;
+                
+                default:
                     Task t = new Task(input);
                     String task = t.getStatusIcon() + " " + t.description;
                     taskList[taskIndex] = task;
                     System.out.println("    added: " + task);
                     taskIndex++;
-                }
-
-                System.out.println(dashLine);
-                System.out.println("Done! Now,\n");
-                System.out.println(promptCommand);
+                    break;
             }
-        } while (!input.equals("bye"));
-        System.out.println(dashLine);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(dashLine);
+
+            // Prompt for next command
+            System.out.println(dashLine);
+            System.out.println("Done! Now,\n");
+            System.out.println(promptCommand);
+        }
     }
+    
 }
