8013b0b73ca2ae230e4646218f109631242a4b99
Kurokishi592 <kennethwongcunwi@gmail.com>
2025-09-17 04:18:19 +0800

    Add level-6: delete

---
+++ src/main/java/kurokishi/command/DeleteCommand.java
@@ -0,0 +1,35 @@
+package kurokishi.command;
+
+import kurokishi.task.TaskList;
+import kurokishi.task.Event;
+import kurokishi.task.Task;
+import kurokishi.exception.InputException;
+import kurokishi.ui.Ui;
+
+public class DeleteCommand implements Command {
+    private final String deleteString;
+
+    public DeleteCommand(String deleteString) { 
+        this.deleteString = deleteString; 
+    }
+
+    @Override
+    public boolean execute(TaskList tasks, Ui ui) throws InputException {
+        if (deleteString == null || deleteString.trim().isEmpty()) {
+            throw new InputException("    [ERROR] Missing item description for 'delete' command.\n" +
+                    "    [SYSTEM NOTICE] Usage: delete <task number>");
+        }
+        try {
+            int index = Integer.parseInt(deleteString.trim()) - 1;
+            Task t = tasks.get(index);
+            tasks.remove(t);
+            ui.printMessage("    [SYSTEM NOTICE] Event task deleted successfully.\n " + "         " + t);
+            ui.printMessage("    [STATUS] Current number of active tasks: " + tasks.size());
+        }
+        catch (NumberFormatException e) {
+            throw new InputException("    [ERROR] Invalid task number format\n" +
+                    "    [SYSTEM NOTICE] Task number must be an integer.");
+        }
+        return false;
+    }
+}
